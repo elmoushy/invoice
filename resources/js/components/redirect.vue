8a2214128dffee793102606679a50adcc67ba165
elmoushy <seif778811@gmail.com>
2025-04-09 06:26:20 +0200

    .

--- resources/js/components/redirect.vue
+++ resources/js/components/redirect.vue
@@ -6,14 +6,10 @@ import { useInvoiceStore } from '../invoice';
 
 export default {
     mounted() {
-
-
         const invoiceStore = useInvoiceStore();
         invoiceStore.$reset(); 
         this.$router.push('/create-invoice/invoice-details');
         
-        // Option 2: Using plain JavaScript (uncomment if not using Vue Router)
-        // window.location.href = '/create-invoice/invoice-details';
     }
 }
 </script>
